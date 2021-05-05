305339c60bde351c9010de5a0941939765263987
Antonio Kim <antoniok@sfu.ca>
2021-05-05 07:02:38 -0700

    Completed Part 1 except 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const Button = ({clickEvent, text}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={clickEvent}>{text}</button>
+  )
+}
+
+const DisplayVote = ({array, index}) => {
+  return (
+    <div>has {array[index]} votes</div>
+  )
+}
+
+const App = () =>{
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+  const [selected, setSelected] = useState(0);
+  const [voted, setVoted] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0));
+  
+  const handleNextClick = () => setSelected(Math.floor(Math.random()*anecdotes.length))
+  const handleVoteClick = () => {
+    let updatedVote = [...voted]
+    updatedVote[selected] += 1
+    
+    setVoted(updatedVote)
+  }
+  
+  
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <DisplayVote array={voted} index={selected} />
+      <Button clickEvent={handleVoteClick} text='vote' />
+      <Button clickEvent={handleNextClick} text='next anecdote' />
+
+      <h1>Anecdote with most votes</h1>
+      <DisplayVote array={voted} index={1} />
     </div>
-  );
+    
+  )
 }
 
 export default App;
